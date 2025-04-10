79a778b8b5ef449b855f6b623f21f14bcc438421
jamnaropani <jamnaropani@gmail.com>
2025-04-10 15:05:34 +0530

    dotfiles: user: gtk: Set font family to Inter

--- user/gtk.nix
+++ user/gtk.nix
@@ -2,6 +2,10 @@
   # GTK
   gtk = {
     enable = true;
+    font = {
+      name = "Inter";
+      package = pkgs.inter;
+    };
     theme = {
       name = "Nordic";
       package = pkgs.nordic;
