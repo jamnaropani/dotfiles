5f2ef07cd25adf8cee844599dc247d4d171dd9b5
jamnaropani <jamnaropani@gmail.com>
2025-04-10 15:09:25 +0530

    dotfiles: user: firefox: Disable irritating first-run stuff and crappy home activity stream page
    https://github.com/Misterio77/nix-config/blob/main/home/gabriel/features/desktop/common/firefox.nix

--- user/programs/firefox.nix
+++ user/programs/firefox.nix
@@ -1,10 +1,48 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   # Firefox
   programs.firefox = {
     enable = true;
     profiles.me = {
       isDefault = true;
       settings = {
+        # Disable irritating first-run stuff
+        "browser.disableResetPrompt" = true;
+        "browser.download.panel.shown" = true;
+        "browser.feeds.showFirstRunUI" = false;
+        "browser.messaging-system.whatsNewPanel.enabled" = false;
+        "browser.rights.3.shown" = true;
+        "browser.shell.checkDefaultBrowser" = false;
+        "browser.shell.defaultBrowserCheckCount" = 1;
+        "browser.startup.homepage_override.mstone" = "ignore";
+        "browser.uitour.enabled" = false;
+        "startup.homepage_override_url" = "";
+        "trailhead.firstrun.didSeeAboutWelcome" = true;
+        "browser.bookmarks.restore_default_bookmarks" = false;
+        "browser.bookmarks.addedImportButton" = true;
+
+        # Disable crappy home activity stream page
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+        "browser.newtabpage.blocked" = lib.genAttrs [
+          # Youtube
+          "26UbzFJ7qT9/4DhodHKA1Q=="
+          # Facebook
+          "4gPpjkxgZzXPVtuEoAL9Ig=="
+          # Wikipedia
+          "eV8/WsSLxHadrTL1gAxhug=="
+          # Reddit
+          "gLv0ja2RYVgxKdp0I5qwvA=="
+          # Amazon
+          "K00ILysCaEq8+bEqV/3nuw=="
+          # Twitter
+          "T9nJot5PurhJSy8n038xGA=="
+        ] (_: 1);
+
         # Auto enable extensions
         "extensions.autoDisableScopes" = 0;
       };
